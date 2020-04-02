88f4a814c6734e31acc98052300f9411aecc4f2b
Eduardo Andre <educdiogo@hotmail.com>
2020-04-02 17:30:43 -0300

    Eventos de mouse

---
+++ moduloJSintermediario/script.js
@@ -0,0 +1,33 @@
+function apertouMouse() {
+    console.log("Apertou o mouse!!!");
+}
+
+function soltouMouse() {
+    console.log("Soltou o mouse!!!");
+}
+
+function mousePassouCima() {
+    console.log("Mouse passou por cima!!!");
+}
+
+function mouseSaiuDeCima() {
+    console.log("Mouse saiu de cima!!!");
+}
+
+function moveuMouse() {
+    console.log("Moveu o mouse!!!");
+}
+
+function clicou() {
+    console.log("Clicou!!!");
+}
+
+function botaoDireito() {
+    console.log("Botao direito");
+
+    return false;
+}
+
+function clickDuplo() {
+    console.log("Clicou duas vezes!!!");
+}
