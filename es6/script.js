25a82816ed6e2914f603ddb24bb5dc8c56affe8a
Eduardo Andre <educdiogo@hotmail.com>
2020-04-08 19:39:00 -0300

    Async e await

--- es6/script.js
+++ es6/script.js
@@ -170,17 +170,43 @@ fazer().then((resposta) => {
 });
 
 //Fetch
-const url = 'https://alunos.b7web.com.br/api/ping';
+/*const url = 'https://alunos.b7web.com.br/api/ping';
 const params = {
-    method: 'POST',
+    method: 'POST'
     body: JSON.stringify({
-        nome: 'Bonieky',
-        idad: 99
-    })
+    nome: 'Bonieky',
+    idad: 99
+        })
 };
 
-fetch(url, params)
+fetch(url)
     .then((r) => r.json())
     .then((json) => {
         console.log(json);
     });
+*/
+// async e await
+function requisitar() {
+    fetch('https://alunos.b7web.com.br/api/ping')
+        .then((r) => r.json())
+        .then((json) => {
+            console.log(json);
+        });
+
+    console.log("Alguma coisa");
+
+}
+
+async function requisitar2() {
+    const url = 'https://alunos.b7web.com.br/api/ping';
+
+    const r = await fetch(url);
+    const json = await r.json();
+
+    console.log(json);
+
+    console.log("Alguma coisa");
+}
+requisitar();
+requisitar2();
+console.log("Texto qualquer");
