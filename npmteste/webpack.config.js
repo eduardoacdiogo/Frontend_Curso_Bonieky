c4e09dc3a6b3112744d80ec5569e0c4bd2c39253
Eduardo Andre <educdiogo@hotmail.com>
2020-04-28 15:36:14 -0300

    Webpack: Refatorando output

--- npmteste/webpack.config.js
+++ npmteste/webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
         segundaTela: './src/segundaTela'
     },
     output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist/assets/js')
+        filename: 'assets/js/[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
     },
     mode: 'production',
     module: {
@@ -18,7 +18,7 @@ module.exports = {
                 use: [{
                     loader: 'file-loader',
                     options: {
-                        name: '../images/[name].[ext]'
+                        name: 'assets/images/[name].[ext]'
                     }
                 }]
             },
