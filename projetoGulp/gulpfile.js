8ab1ced5af4b3cbbdc8fb19500cf7b6d46d0be30
Eduardo Andre <educdiogo@hotmail.com>
2020-04-28 16:55:31 -0300

    Gulp: rename

--- projetoGulp/gulpfile.js
+++ projetoGulp/gulpfile.js
@@ -1,4 +1,4 @@
-const { series, parallel } = require('gulp');
+/*const { series, parallel } = require('gulp');
 
 function clean(cb) {
     cb();
@@ -31,8 +31,16 @@ exports.build = series(
 //exports.build = parallel(helloTask, javascript, css);
 //exports.build = series(helloTask, javascript, css);
 
-/*
+
 exports.css = css;
 exports.js = javascript;
 exports.default = helloTask;
 */
+
+const { src, dest } = require('gulp');
+const rename = require('gulp-rename');
+exports.default = function() {
+    return src('src/js/*.js')
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe(dest('dist/'));
+}
