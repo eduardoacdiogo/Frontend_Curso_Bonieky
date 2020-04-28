6f662e9403020da0711fd44f10f0113896980b59
Eduardo Andre <educdiogo@hotmail.com>
2020-04-27 23:51:28 -0300

    WebPack: Criando componentes

--- npmteste/src/index.js
+++ npmteste/src/index.js
@@ -1,3 +1,8 @@
+import $ from 'jquery';
+import './css/style.css';
+import CameraGallery from './images/chave.jpg';
+import Botao from './componentes/botao/index.js';
+
 const moment = require('moment');
 
 moment.locale('pt-br');
@@ -7,14 +12,20 @@ document.getElementById("dia").innerHTML = "O dia de semana eh " + dia + ".";
 
 //console.log(`O dia da semana é: ${dia}`);
 
-import $ from 'jquery';
-import './style.css';
-import CameraGallery from './images/chave.jpg';
-
 $(function() {
-    $('#botao').on('click', function() {
+
+    let botao = new Botao();
+
+    botao.setTitle('Testador');
+    botao.setClick(function() {
+        alert("Clicou!");
+    });
+    $('.area').html(botao.render());
+});
+$(function() {
+    $('#bootao').on('click', function() {
         $('h1').html('Ola Mundo Alterado!!');
-        $(this).addClass('botao');
+        $(this).addClass('bootao');
         $('#imagem').attr('src', CameraGallery);
 
     });
