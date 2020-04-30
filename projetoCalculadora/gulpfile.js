e8bbf70d8e0482fa7984f32d98c57a305c3895ba
Eduardo Andre <educdiogo@hotmail.com>
2020-04-30 14:57:32 -0300

    Projeto Calculadora 2/5

---
+++ projetoCalculadora/gulpfile.js
@@ -0,0 +1,33 @@
+const { src, dest, serie, parallel } = require('gulp');
+const rename = require('gulp-rename');
+const cssimport = require('gulp-cssimport');
+const minifyjs = require('gulp-uglify');
+const minifycss = require('gulp-uglifycss');
+const sass = require('gulp-sass');
+const babel = require('gulp-babel');
+
+function base() {
+    return src('src/templates/*.html')
+        .pipe(dest('public/'));
+}
+
+function javascript() {
+    return src('src/js/*.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(minifyjs())
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe(dest('public/asstes/js'));
+}
+
+function css() {
+    return src('src/css/*.css')
+        .pipe(cssimport())
+        .pipe(sass())
+        .pipe(minifycss())
+        .pipe(rename({ extname: '.min.css' }))
+        .pipe(dest('public/assets/css'));
+}
+
+exports.default = parallel(base, javascript, css);
