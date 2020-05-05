ae4c5e7ace2bd8c3e937822195453abf417dce6e
Eduardo Andre <educdiogo@hotmail.com>
2020-05-05 13:00:05 -0300

    JSX React.createElement

---
+++ react/script.js
@@ -0,0 +1,13 @@
+/*let elemento = <h1 className="titulo"> Olá, Mundo!</h1>;*/
+
+/*
+let titulo = React.createElemento(
+    'h1', { className = 'titulo' },
+    'Olá, Mundo 2.0'
+);
+*/
+
+let titulo = < h1 className = "titulo" > Olá,
+    Mundo 3.0 < /h1>;
+
+ReactDOM.render(titulo, document.getElementById('app'));
