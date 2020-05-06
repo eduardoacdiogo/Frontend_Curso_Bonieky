863d79411f088bbf4aff7b4d5446cf90a06fbeff
Eduardo Andre <educdiogo@hotmail.com>
2020-05-06 20:59:19 -0300

    ReactJS: introdução a componentes e props

--- react/script.js
+++ react/script.js
@@ -11,14 +11,32 @@ ReactDOM.render(titulo, document.getElementById('app'));
 */
 function mostrarHora() {
 
-    let elemento = < div >
-        <
-        h1 > Olá,
-        Usuário < /h1>  <
-    h3 className = "hora" > Agora são: { new Date().toLocaleTimeString() } < /h3>  < /
-    div >
+    let elemento = <div>
+        <h1> Olá, Usuário </h1>  
+        <h3 className = "hora" > Agora são: { new Date().toLocaleTimeString() } </h3>  
+        </div>
 
         ReactDOM.render(elemento, document.getElementById('app'));
 }
 
 setInterval(mostrarHora, 1000);
+
+function Bemvindo(props) {
+    return ( 
+        <div>
+        <h1> Seja bem vindo </h1>
+        <h3> {props.nome} </h3> 
+        </div>
+    );
+}
+let elemento2 = ( 
+    <div>
+    <Bemvindo nome="Eduardo" />
+
+    <hr/>
+
+    <Bemvindo nome="Andre"/>
+    </div>
+);
+
+ReactDOM.render(elemento2, document.getElementById('app2'));
